d162a2f2c77c0711b7d5c78d0060c5964dc3361b
Wanessa Witkowska <128226892+Shugisia@users.noreply.github.com>
2023-06-24 17:35:44 +0200

    Add files via upload

---
+++ zadanie01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int czas = 3661; //czas w sekundach
+
+        string sformatowanyCzas = FormatujCzas(czas);
+        Console.WriteLine(sformatowanyCzas);
+    }
+
+    static string FormatujCzas(int sformatowanyCzas)
+    {
+        int godzina = czas / 3600;
+        int minuty = (czas % 3600) / 60;
+        int sekundy = (czas % 3600) % 60;
+
+        string sformatowanaGodzina = godzina.ToString("D2");
+        string sformatowanaMinuta = minuty.ToString("D2");
+        string sformatowanaSekunda = sekundy.ToString("D2");
+
+        return $"{sformatowanaGodzina}:{sformatowanaMinuta}:{sformatowanaSekunda}";
+    }
+}
